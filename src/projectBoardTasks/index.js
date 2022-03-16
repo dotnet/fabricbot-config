4298bae758c2b5fcb6c7615114db3fa7642e76bd
Jeff Handley <jeffhandley@users.noreply.github.com>
2022-03-16 11:49:26 -0700

    Disable the champion assigned and triage started tasks while bugs are addressed

--- src/projectBoardTasks/index.js
+++ src/projectBoardTasks/index.js
@@ -1,19 +1,15 @@
 const issueMovedToAnotherArea = require("./issueMovedToAnotherArea");
 const issueNeedsTriage = require("./issueNeedsTriage");
-const issueTriageStarted = require("./issueTriageStarted");
 const issueNeedsFurtherTriage = require("./issueNeedsFurtherTriage");
 const issueTriaged = require("./issueTriaged");
 const pullRequestMovedToAnotherArea = require("./pullRequestMovedToAnotherArea");
 const pullRequestNeedsChampion = require("./pullRequestNeedsChampion");
-const pullRequestChampionAssigned = require("./pullRequestChampionAssigned");
 
 module.exports = (options) => [
   ...issueMovedToAnotherArea(options),
   ...issueNeedsTriage(options),
-  ...issueTriageStarted(options),
   ...issueNeedsFurtherTriage(options),
   ...issueTriaged(options),
   ...pullRequestMovedToAnotherArea(options),
-  ...pullRequestNeedsChampion(options),
-  ...pullRequestChampionAssigned(options)
+  ...pullRequestNeedsChampion(options)
 ];
