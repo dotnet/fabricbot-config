193b13213a92d1b56f8af201941713eaff3dc188
Jeff Handley <jeffhandley@users.noreply.github.com>
2022-04-28 23:43:04 -0700

    Refactor and debug PR championship

--- src/projectBoardTasks/pullRequestChampionAssigned.js
+++ src/projectBoardTasks/pullRequestChampionAssigned.js
@@ -24,80 +24,43 @@ module.exports = ({podName, podAreas, podMembers}) => podMembers.map(({name, use
           "name": "isOpen",
           "parameters": {}
         },
+        (Array.isArray(podAreas) && {
+          "operator": "or",
+          "operands": podAreas.map(label => ({
+            "name": "hasLabel",
+            "parameters": { label }
+          }))
+        }),
+        {
+          "name": "isAssignedToUser",
+          "parameters": { user },
+        },
         {
           "operator": "or",
           "operands": [
             {
-              "operator": "and",
+              "operator": "not",
               "operands": [
                 {
-                  "name": "isInProjectColumn",
+                  "name": "isInProject",
                   "parameters": {
                     "projectName": `Area Pod: ${podName} - PRs`,
-                    "columnName": "Needs Champion",
                     "isOrgProject": true
                   }
-                },
-                {
-                  "name": "isAction",
-                  "parameters": {
-                    "action": "assigned"
-                  }
-                },
-                {
-                  "name": "isAssignedToUser",
-                  "parameters": { user }
                 }
               ]
             },
             {
-              "operator": "and",
-              "operands": [
-                (Array.isArray(podAreas) && {
-                  "operator": "or",
-                  "operands": podAreas.map(label => ({
-                    "name": "hasLabel",
-                    "parameters": { label }
-                  }))
-                }),
-                {
-                  "operator": "not",
-                  "operands": [
-                    {
-                      "name": "isInProject",
-                      "parameters": {
-                        "projectName": `Area Pod: ${podName} - PRs`,
-                        "isOrgProject": true
-                      }
-                    }
-                  ]
-                },
-                {
-                  "name": "isActivitySender",
-                  "parameters": { user },
-                },
-                {
-                  "operator": "or",
-                  "operands": [
-                    {
-                      "name": "isAction",
-                      "parameters": {
-                        "action": "opened"
-                      }
-                    },
-                    {
-                      "name": "isAction",
-                      "parameters": {
-                        "action": "reopened"
-                      }
-                    }
-                  ]
-                }
-              ].filter(op => !!op) // We will have a falsy element in the array of we're not filtering by area label
+              "name": "isInProjectColumn",
+              "parameters": {
+                "projectName": `Area Pod: ${podName} - PRs`,
+                "columnName": "Done",
+                "isOrgProject": true
+              }
             }
           ]
         }
-      ]
+      ].filter(op => !!op) // We will have a falsy element in the array of we're not filtering by area label
     }
   }
 }));
--- src/projectBoardTasks/pullRequestNeedsChampion.js
+++ src/projectBoardTasks/pullRequestNeedsChampion.js
@@ -20,6 +20,10 @@ module.exports = ({podName, podAreas, podMembers}) => [{
     "conditions": {
       "operator": "and",
       "operands": [
+        {
+          "name": "isOpen",
+          "parameters": {}
+        },
         (Array.isArray(podAreas) && {
           "operator": "or",
           "operands": podAreas.map(label => ({
@@ -29,26 +33,35 @@ module.exports = ({podName, podAreas, podMembers}) => [{
         }),
         {
           "operator": "not",
-          "operands": [
-            {
-              "name": "isInProject",
-              "parameters": {
-                "projectName": `Area Pod: ${podName} - PRs`,
-                "isOrgProject": true
-              }
-            }
-          ]
-        },
-        {
-          "operator": "not",
           "operands": podMembers.map(({user}) => ({
             "name": "isAssignedToUser",
             "parameters": { user }
           }))
         },
         {
-          "name": "isOpen",
-          "parameters": {}
+          "operator": "or",
+          "operands": [
+            {
+              "operator": "not",
+              "operands": [
+                {
+                  "name": "isInProject",
+                  "parameters": {
+                    "projectName": `Area Pod: ${podName} - PRs`,
+                    "isOrgProject": true
+                  }
+                }
+              ]
+            },
+            {
+              "name": "isInProjectColumn",
+              "parameters": {
+                "projectName": `Area Pod: ${podName} - PRs`,
+                "columnName": "Done",
+                "isOrgProject": true
+              }
+            }
+          ]
         }
       ].filter(op => !!op) // We will have a falsy element in the array of we're not filtering by area label
     }
