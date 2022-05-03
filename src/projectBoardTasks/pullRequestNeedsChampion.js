0cd21d15590bd9aa0d1e2fcc928dc69d036d019d
Jeff Handley <jeffhandley@users.noreply.github.com>
2022-05-02 21:27:59 -0700

    Refactor PR Needs Champion into 2 tasks (to fix logic issue)

--- src/projectBoardTasks/pullRequestNeedsChampion.js
+++ src/projectBoardTasks/pullRequestNeedsChampion.js
@@ -1,123 +1,188 @@
-module.exports = ({podName, podAreas, podMembers}) => [{
-  "taskType": "trigger",
-  "capabilityId": "IssueResponder",
-  "subCapability": "PullRequestResponder",
-  "version": "1.0",
-  "config": {
-    "taskName": `[Area Pod: ${podName} - PRs] Needs Champion`,
-    "actions": [
-      {
-        "name": "removeFromProject",
-        "parameters": {
-          "projectName": `Area Pod: ${podName} - PRs`,
-          "isOrgProject": true
-        }
-      },
-      {
-        "name": "addToProject",
-        "parameters": {
-          "projectName": `Area Pod: ${podName} - PRs`,
-          "columnName": "Needs Champion",
-          "isOrgProject": true
-        }
-      }
-    ],
-    "eventType": "pull_request",
-    "eventNames": ["pull_request"],
-    "conditions": {
-      "operator": "and",
-      "operands": [
-        // The PR is open
+module.exports = ({podName, podAreas, podMembers}) => [
+  {
+    "taskType": "trigger",
+    "capabilityId": "IssueResponder",
+    "subCapability": "PullRequestResponder",
+    "version": "1.0",
+    "config": {
+      "taskName": `[Area Pod: ${podName} - PRs] New PR Needs Champion`,
+      "actions": [
         {
-          "name": "isOpen",
-          "parameters": {}
-        },
-        // And it has one of the pod's area labels
-        (Array.isArray(podAreas) && {
-          "operator": "or",
-          "operands": podAreas.map(label => ({
-            "name": "hasLabel",
-            "parameters": { label }
-          }))
-        }),
-        {
-          "operator": "and",
-          // And it's not assigned to any of the pod members
-          "operands": [
-            {
-              "operator": "and",
-              "operands": podMembers.map(({user}) => ({
-                "operator": "not",
-                "operands": [
-                  {
-                    "name": "isAssignedToUser",
-                    "parameters": { user }
-                  }
-                ]
-              }))
-            },
-            // (It belongs to this pod, and it's not yet assigned to a pod member, and...)
-            {
-              "operator": "or",
-              "operands": [
-                // The PR was either not being opened right now (in which case it wouldn't have an assignee yet)
-                {
-                  "operator": "not",
-                  "operands": [
-                    {
-                      "name": "isAction",
-                      "parameters": {
-                        "action": "opened"
-                      }
-                    }
-                  ]
-                },
-                // Or (it was just opened), and it was not opened by any of the pod members
-                {
-                  "operator": "and",
-                  "operands": podMembers.map(({user}) => ({
-                    "operator": "not",
-                    "operands": [
-                      {
-                        "name": "isActivitySender",
-                        "parameters": { user }
-                      }
-                    ]
-                  }))
-                }
-              ]
+          "name": "addToProject",
+          "parameters": {
+            "projectName": `Area Pod: ${podName} - PRs`,
+            "columnName": "Needs Champion",
+            "isOrgProject": true
+          }
+        }
+      ],
+      "eventType": "pull_request",
+      "eventNames": ["pull_request"],
+      "conditions": {
+        "operator": "and",
+        "operands": [
+          // The PR was just opened
+          {
+            "name": "isAction",
+            "parameters": {
+              "action": "opened"
             }
-          ]
-        },
-        // (The PR belongs to the pod, it's not yet assigned to a pod member, nor was it just opened by a pod member, and...)
-        {
-          "operator": "or",
-          "operands": [
-            // It's not yet in the project
+          },
+          // And it has one of the pod's area labels
+          (Array.isArray(podAreas) && {
+            "operator": "or",
+            "operands": podAreas.map(label => ({
+              "name": "hasLabel",
+              "parameters": { label }
+            }))
+          }),
+          // And it's not assigned to a pod member already, nor was it opened by a pod member
+          podMembers.map(({user}) => [
             {
               "operator": "not",
               "operands": [
                 {
-                  "name": "isInProject",
-                  "parameters": {
-                    "projectName": `Area Pod: ${podName} - PRs`,
-                    "isOrgProject": true
-                  }
+                  "name": "isAssignedToUser",
+                  "parameters": { user }
                 }
               ]
             },
-            // Or it's in the project, but it's in the Done column
             {
-              "name": "isInProjectColumn",
-              "parameters": {
-                "projectName": `Area Pod: ${podName} - PRs`,
-                "columnName": "Done",
-                "isOrgProject": true
-              }
+              "operator": "not",
+              "operands": [
+                {
+                  "name": "isActivitySender",
+                  "parameters": { user }
+                }
+              ]
             }
-          ]
+          ]),
+          // (The PR was just opened, it belongs to the pod, and it wasn't assigned to or opened by a pod member, and...)
+          {
+            "operator": "or",
+            "operands": [
+              // It's not yet in the project
+              {
+                "operator": "not",
+                "operands": [
+                  {
+                    "name": "isInProject",
+                    "parameters": {
+                      "projectName": `Area Pod: ${podName} - PRs`,
+                      "isOrgProject": true
+                    }
+                  }
+                ]
+              },
+              // Or it's in the project, but it's in the Done column
+              {
+                "name": "isInProjectColumn",
+                "parameters": {
+                  "projectName": `Area Pod: ${podName} - PRs`,
+                  "columnName": "Done",
+                  "isOrgProject": true
+                }
+              }
+            ]
+          }
+        ].filter(op => !!op) // We will have a falsy element in the array of we're not filtering by area label
+      }
+    }
+  },
+  {
+    "taskType": "trigger",
+    "capabilityId": "IssueResponder",
+    "subCapability": "PullRequestResponder",
+    "version": "1.0",
+    "config": {
+      "taskName": `[Area Pod: ${podName} - PRs] Updated PR Needs Champion`,
+      "actions": [
+        {
+          "name": "removeFromProject",
+          "parameters": {
+            "projectName": `Area Pod: ${podName} - PRs`,
+            "isOrgProject": true
+          }
+        },
+        {
+          "name": "addToProject",
+          "parameters": {
+            "projectName": `Area Pod: ${podName} - PRs`,
+            "columnName": "Needs Champion",
+            "isOrgProject": true
+          }
         }
-      ].filter(op => !!op) // We will have a falsy element in the array of we're not filtering by area label
+      ],
+      "eventType": "pull_request",
+      "eventNames": ["pull_request"],
+      "conditions": {
+        "operator": "and",
+        "operands": [
+          // The PR is open
+          {
+            "name": "isOpen",
+            "parameters": {}
+          },
+          // And it wasn't just opened
+          {
+            "operator": "not",
+            "operands": [
+              {
+                "name": "isAction",
+                "parameters": {
+                  "action": "opened"
+                }
+              }
+            ]
+          },
+          // And it has one of the pod's area labels
+          (Array.isArray(podAreas) && {
+            "operator": "or",
+            "operands": podAreas.map(label => ({
+              "name": "hasLabel",
+              "parameters": { label }
+            }))
+          }),
+          // And it's not assigned to a pod member already
+          podMembers.map(({user}) => ({
+            "operator": "not",
+            "operands": [
+              {
+                "name": "isAssignedToUser",
+                "parameters": { user }
+              }
+            ]
+          })),
+          // (The PR is open but wasn't just opened, it belongs to the pod, and it's not yet assigned to a pod member, and...)
+          {
+            "operator": "or",
+            "operands": [
+              // It's not yet in the project
+              {
+                "operator": "not",
+                "operands": [
+                  {
+                    "name": "isInProject",
+                    "parameters": {
+                      "projectName": `Area Pod: ${podName} - PRs`,
+                      "isOrgProject": true
+                    }
+                  }
+                ]
+              },
+              // Or it's in the project, but it's in the Done column
+              {
+                "name": "isInProjectColumn",
+                "parameters": {
+                  "projectName": `Area Pod: ${podName} - PRs`,
+                  "columnName": "Done",
+                  "isOrgProject": true
+                }
+              }
+            ]
+          }
+        ].filter(op => !!op) // We will have a falsy element in the array of we're not filtering by area label
+      }
     }
   }
-}];
+];
