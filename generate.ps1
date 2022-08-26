5afa6b71e1106137ef40ad2a2fd518e46f93081d
Eric StJohn <ericstj@microsoft.com>
2022-08-26 10:41:22 -0700

    Remove roslyn-analyzers from scripts

--- generate.ps1
+++ generate.ps1
@@ -3,4 +3,3 @@ node src/merge.js ./generated/fabricbot-config.json .github/fabricbot.json
 node src/merge.js ./generated/runtime.json          ../runtime/.github/fabricbot.json
 node src/merge.js ./generated/machinelearning.json  ../machinelearning/.github/fabricbot.json
 node src/merge.js ./generated/dotnet-api-docs.json  ../dotnet-api-docs/.github/fabricbot.json
-node src/merge.js ./generated/roslyn-analyzers.json ../roslyn-analyzers/.github/fabricbot.json
