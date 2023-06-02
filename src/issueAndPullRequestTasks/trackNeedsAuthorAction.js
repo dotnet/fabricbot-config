32944857c01289f87e2a34c4de3aca01321f478a
Jeff Handley <jeffhandley@users.noreply.github.com>
2023-06-01 17:33:12 -0700

    Revert "Do not add the needs-further-triage label if untriaged label exists (#74)" (#75)
    This reverts commit 64c042f3c696922b00e08d2113b71e31e241f91b.

--- src/issueAndPullRequestTasks/trackNeedsAuthorAction.js
+++ src/issueAndPullRequestTasks/trackNeedsAuthorAction.js
@@ -126,17 +126,6 @@ module.exports = () => [
           {
             "name": "isOpen",
             "parameters": {}
-          },
-          {
-            "operator": "not",
-            "operands": [
-              {
-                "name": "hasLabel",
-                "parameters": {
-                  "label": "untriaged"
-                }
-              }
-            ]
           }
         ]
       }
